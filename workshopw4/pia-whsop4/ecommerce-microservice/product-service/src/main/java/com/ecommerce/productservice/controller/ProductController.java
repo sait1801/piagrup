2a041ac956c7d0d5606c92887d94d05463bb61a8
Sait Burak Yucekaya <sait.burak20@gmail.com>
2024-08-24 17:24:17 +0300

    new workshops added

---
+++ workshopw4/pia-whsop4/ecommerce-microservice/product-service/src/main/java/com/ecommerce/productservice/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.ecommerce.productservice.controller;
+
+import com.ecommerce.productservice.model.Product;
+import com.ecommerce.productservice.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+}
